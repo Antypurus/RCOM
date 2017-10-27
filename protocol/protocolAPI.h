1bfb3c68306ae3c731b2339b63b9d1d91e9a9012
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-27 16:34:59 +0100

    implemented interface protocols

---
+++ protocol/protocolAPI.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct sideControll{
+    int side;
+};
+
+struct sideControll g_side;
+
+#define TRANSMITER 0
+#define RECEIVER 1
+
+//NEEDS DOCUMENTATION
+int llopen(int porta,int side);//llopen
+
+//NEEDS DOCUMENTATION
+int llread(int fd,char* buffer);//llread
+
+//NEEDS DOCUMENTATION
+int llwrite(int fd,char*buffer,int lenght);//llwrite
+
+//NEEDS DOCUMENTATION
+int llclose(int fd);//llclose
