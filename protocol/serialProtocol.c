56e40e659c79d28efdf85da6a084ecc5e175e209
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-29 16:35:21 +0000

    syntax issues fixing

--- protocol/serialProtocol.c
+++ protocol/serialProtocol.c
@@ -896,7 +896,7 @@ unsigned char sendData(unsigned int fd, const unsigned char data[], unsigned int
 
 void timeoutHandler(int sig)
 {
-    if (hasReceived = FALSE)
+    if (hasReceived == FALSE)
     {
         printf("[LOG]@timeoutHandle\tTimeout signal occurred\n");
         if (g_ctrl.retryCounter >= MAX_TIMEOUT)
