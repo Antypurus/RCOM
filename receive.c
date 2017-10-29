c4d9bb4241c0924387cff634238f80cd39e79408
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-10-29 21:39:49 +0000

    preliminary test function ready to use

--- receive.c
+++ receive.c
@@ -9,16 +9,20 @@ int main()
 {
 	char *buf = NULL;
 	int fd = llopen(0, RECEIVER);
-	int rd = llread(fd, &buf);
-	if (rd == 0)
-	{
-		printf("unable to read\n");
-		int ret = llclose(fd);
-		if (ret == 1)
-		{
-			printf("closed\n");
+	int rd;
+	while(buf==NULL || strcmp(bud,"DC")){
+		rd = llread(fd, &buf);
+		if(rd==0){
+			printf("unable to read\n");
+			int ret = llclose(fd);
+			if (ret == 1)
+			{
+				printf("closed\n");
+			}
+			return -1;
+		}else{
+			printf("read %s , size %d",buf,rd);
 		}
-		return -1;
 	}
 	if (!strcmp("DC", buf))
 	{
--- send.c
+++ send.c
@@ -5,8 +5,22 @@
 #include"protocol/serialProtocol.h"
 
 int main(){
+	char noStuff[] = "Hello My Good Friend";
+	char stuff[4];
+	stuff[0]=0x65;
+	stuff[1]=0x7E;
+	stuff[2]=0x7D;
+	stuff[3]=0x65;
+
 	int fd = llopen(0,TRANSMITER);
-	sleep(1);
+	printf("Sending %s\n",noStuff);
+	int read = llwrite(fd,noStuff,sizeof(noStuff));
+	printf("Wrote %d\n",read);
+
+	printf("Sending %s\n",stuff);
+	read = llwrite(fd,stuff,sizeof(stuff));
+	printf("Wrote %d\n",read);
+
 	llclose(fd);
 	return 0;
 }
