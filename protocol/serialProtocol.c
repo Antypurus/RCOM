cde3443563cd6f405135310d0346aa46b6274b35
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-30 00:53:22 +0000

    bugfix

--- protocol/serialProtocol.c
+++ protocol/serialProtocol.c
@@ -1177,7 +1177,7 @@ unsigned char *extractDataFromFrame(unsigned char *data, unsigned int *sizeOf)
         printf("[SUCCESS]@extract\tSuccessfully allocated buffer for data\n");
     }
 
-    memmove(buff, data + 4, sz);
+    memmcpy(buff, data + 4, sz);
     *sizeOf = sz;
     printf("[SUCCESS]@extract\tData Extraction Complete\n");
     return buff;
