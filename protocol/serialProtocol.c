274f096cce5f1458327e55186e185e92e56f5e16
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-10-30 00:14:40 +0000

    log

--- protocol/serialProtocol.c
+++ protocol/serialProtocol.c
@@ -271,8 +271,11 @@ unsigned char moveDataToFrames(unsigned char **frames, const unsigned char data[
             printf("[SUCCESS]@dataMv\tByte stuffing complete,obtained data %s\n",stuffed);
         }
         g_ctrl.allocError = 0;
+        printf("flag byte %d",frames[i][sizeOf-1]);
         moveInformationToFrame(frames[i], stuffed, s_size);     //moves the chunk of data into the frame
+        printf("flag byte %d",frames[i][sizeOf-1]);
         frames[i][sizeOf - 2] = calculateBCC2(info[i], s_size); //sets the BCC for the data chunk that was moved
+        printf("flag byte %d",frames[i][sizeOf-1]);
     }
     printf("[SUCCESS]@dataMv\tInformation and BCC2 have been set for the frames\n");
     return 1;
